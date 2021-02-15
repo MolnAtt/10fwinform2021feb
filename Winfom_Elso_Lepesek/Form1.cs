f48905970ea0a6e67c6d5c50792b32746866d014
MolnAtt <molnar.attila@szlgbp.hu>
2021-02-15 14:04:22 +0100

    első óra anyaga

--- Winfom_Elso_Lepesek/Form1.cs
+++ Winfom_Elso_Lepesek/Form1.cs
@@ -12,9 +12,46 @@ namespace Winfom_Elso_Lepesek
 {
 	public partial class Form1 : Form
 	{
+
+		private Label cimke = new Label();
+		private Button gomb = new Button();
+
+		private Timer metronóm = new Timer();
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			// MessageBox.Show("asdéfklj");
+
+			cimke.Text = "hello world";
+			Controls.Add(cimke);
+
+			cimke.Location = new Point(50,50);
+
+			gomb.Text = "ez egy gomb";
+			Controls.Add(gomb);
+
+			gomb.Location = new Point(200, 50);
+
+			gomb.Click += Köszönés;
+
+			metronóm.Interval = 100;
+
+			metronóm.Tick += Ketyeg;
+
+
 		}
+		int i = 0;
+
+		private void Ketyeg(object sender, EventArgs e)
+		{
+			cimke.Text = i++.ToString();
+		}
+		private void Köszönés (object sender, EventArgs e)
+		{	
+			metronóm.Start();
+		}
+
 	}
 }
